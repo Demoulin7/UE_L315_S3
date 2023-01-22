4b69cff1e8fdee93091be4202d772773a2d3d8f6
demoulin7 <bosonhiggs@outlook.fr>
2023-01-22 19:37:22 +0100

    mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,15 @@ import { Users } from './users/entities/user.entity';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [Users],
-      synchronize: true,
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
     }),
     UsersModule
   ],
